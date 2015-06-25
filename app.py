e972abc2e91f8f8704e29b2693aa54dc72423b7b
Guillaume MOURIER <gmourier@tsl.corp.thales>
2015-06-25 13:11:53 +0200

    Work

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-	return "Hello World!"
+  backend_id = os.environ['BACKEND']
+	return 'Successfull reach flask server : %s' % (backend_id)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(host='0.0.0.0')
